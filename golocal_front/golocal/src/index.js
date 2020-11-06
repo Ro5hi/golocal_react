5dc59403fe476992f3829bac5ebd637b4a46ab49
Ro5hi <slen.detablan@gmail.com>
2020-11-06 15:50:41 -0700

    fixed file structure, ran yarn start, rendered

--- golocal_front/golocal/src/index.js
+++ golocal_front/golocal/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from 'src/reportWebVitals';
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import { Provider } from 'react-redux';
-import App from 'components/App.js';
-import 'styles/index.css';
+import App from './components/App.js';
+import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +13,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
