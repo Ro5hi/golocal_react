524be2cb8c6212de1d58c1dc50b1e089517e0152
Ro5hi <slen.detablan@gmail.com>
2020-11-06 16:59:55 -0700

    debugging home page components

--- golocal_front/golocal/src/components/App.js
+++ golocal_front/golocal/src/components/App.js
@@ -3,27 +3,10 @@ import React from 'react';
 
     const App = () => {
         return (
-            <Wrapper>
-                <Container>
-                    <h1>testing react app</h1>
-                </Container>    
-            </Wrapper>
+            <div id="logo">
+                <h1>GO LOCAL</h1>
+            </div>
         );
     };
-
-    const Container = styled.div`
-        background: #FBEABE;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-    `;
-
-    const Wrapper = styled.div`
-        background-position:center;
-        width: 100%;
-        height: 100%;
-    `;
 
     export default App
