950ef407c3c33e3d84fb75e74cc07b58e4fd9492
Ro5hi <slen.detablan@gmail.com>
2020-11-08 13:54:52 -0700

    updated post reducer

--- golocal_front/golocal/src/reducers/postForm.js
+++ golocal_front/golocal/src/reducers/postForm.js
@@ -2,6 +2,7 @@ import {
     GET_POSTS, GET_POSTS_SUCCESS, GET_POSTS_FAILED,
     GET_POST, GET_POST_SUCCESS, GET_POST_FAILED,
     UPLOAD_POST, UPLOAD_SUCCESS, UPLOAD_FAILED,
+    DELETE_POST, POST_DELETED, DELETE_FAILED,
     VALIDATE_POST_PARAMS, POST_PARAMS_SUCCESS, POST_PARAMS_FAILURE,
     RESET_POST_FORM
 } from '../actions/posts';
@@ -10,11 +11,14 @@ import {
     {Posts: { posts: [], error: null, loading: false},
     newPost:{post: null, error: null, loading: false},
     activePost:{post: null, error: null, loading: false},
+    deletedPost: {post: null, error: null, loading: false}
     };
 
     export default function(state = initialState, action) {
         let error;
         switch(action.type) {
+            
+            // Fetch
             case GET_POSTS:
                 return { ...state, Posts: {posts:[], error: null, loading: true} };
             case GET_POSTS_SUCCESS:
@@ -28,6 +32,8 @@ import {
             case GET_POST_FAILED:
                 error = action.payload || {message: action.payload.message};
                 return { ...state, Posts: {posts:[], error: error, loading: false} };
+
+            // Post
             case UPLOAD_POST:
                 return { ...state, newPost: {...state.newPost, loading: true} };
             case UPLOAD_POST_SUCCESS:
@@ -35,6 +41,17 @@ import {
             case UPLOAD_POST_FAILED:
                 error = action.payload || {message: action.payload.message};
                 return { ...state, newPost: {post: null, error: error, loading: false} };
+
+            // Delete
+            case DELETE_POST:
+                return { ...state, deletedPost: {...state.deletedPost, loading: true} };
+            case POST_DELETED:
+                return { ...state, deletedPost: {post: action.payload, error: null, loading: false} };
+            case DELETE_FAILED:
+                error = action.payload || {message: action.payload.message};
+                return { ...state, deletedPost: {post: null, error: null, loading: false} };
+            
+            // Validate
             case VALIDATE_POST_PARAMS:
                 return { ...state, newPost:{...state.newPost, error: null, loading: true} };
             case POST_PARAMS_SUCCESS:
