324dd259a32dc15976937593ad3843dbf834ca95
Ro5hi <slen.detablan@gmail.com>
2020-11-08 10:39:56 -0700

    debugging logout & user sessions

--- golocal_front/golocal/src/actions/currentuser.js
+++ golocal_front/golocal/src/actions/currentuser.js
@@ -1,6 +1,7 @@
 import { resetLoginForm } from './loginform.js'
 import { resetSignupForm } from './sigupform.js'
 
+    // See current user 
     export const currentUser = user => {
         return {
             type: "CURRENT_USER",
@@ -8,15 +9,17 @@ import { resetSignupForm } from './sigupform.js'
         }
     }
 
+    // Clear user session
     export const clearSession = () => {
         return {
             type: "CLEAR_SESSION"
         }
     }
 
+    // POST user login
     export const login = (credentials, history) => {
         return dispatch => {
-            return fetch('https://localhost:3001/api/v1/login', {
+            return fetch('http://localhost:3001/api/v1/login', {
                 credentials: "include",
                 method: "POST",
                 headers: {
@@ -26,7 +29,7 @@ import { resetSignupForm } from './sigupform.js'
             })
             .then(r => r.json())
             .then(response => {
-                if (response.error) 
+                if (response.error) { 
                 alert(response.error)
             } else {
                 dispatch(currentUser(response.data))
@@ -34,5 +37,67 @@ import { resetSignupForm } from './sigupform.js'
                 history.push('/')
             }
             })
+            .catch(console.log)
+        }
+    }
+
+    // POST user signup
+    export const register = (credentials, history) => {
+        return dispatch => {
+            const userInfo = {
+                user: credentials
+            }
+            return fetch("http://localhost:3001/api/v1/signup", {
+                credentials: "include",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(profile)
+            })
+            .then(r => r.json())
+            .then(response => {
+                if (response.error) {
+                    alert(response.error)
+                } else {
+                    dispatch(currentUser(response.data))
+                    dispatch(resetSignupForm)
+                    history.push('/')
+                }
+            })
+            .catch(console.log)
+        }
+
+        // Logout user 
+        export const logout = event => {
+            return dispatch => {
+                dispatch(clearSession())
+                return fetch ('http://localhost:3001/api/v1/logout'), {
+                    credentials: "include",
+                    method: "DELETE"
+                }
+            }
+        }
+
+        // Get user session
+        export const getSession = () => {
+            return dispatch => {
+                return fetch("http://localhost:3001/api/v1/get_session", {
+                    credentials: "include",
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                })
+                .then(r => r.json())
+                .then(response => {
+                    if (response.error) {
+                        alert(response.error)
+                    } else {
+                        dispatch(currentUser(response.data))
+                    }
+                })
+                .catch(console.log)
+            }
         }
     }
---
+++ golocal_front/golocal/src/components/Logout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logout } from '../actions/currentuser.js'
+
+    const loggingOut = ({ logout, history }) => {
+        return (
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            logout()
+            history.push('/')
+            }
+        }>
+            <input type="submit" value="Log Out"/>
+        </form>
+        )
+    }
+
+    export default withRouter(connect(null { logout } )(loggingOut))
