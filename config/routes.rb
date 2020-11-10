8c374613b53690e5de72dd349d8a6864c9a9fc3f
= <=>
2020-11-10 15:38:28 -0700

    update backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/user#create"
+  post "/api/v1/newpost", to: "api/v1/posts#create"
+  get "/api/v1/profile", to: "api/v1/users#show"
+  get "/api/1/get_session", to: "api/v1/sessions#get_session"
+  delete "/api/v1/logout" to: "/api/v1/sessions#destroy"
 
   namespace :api do 
     namespace :v1 do 
