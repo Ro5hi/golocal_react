793b8b9b05f3cb15f277dca187d9fde6e8d5ed9a
= <=>
2020-11-14 19:13:18 -0700

    removed comments db, fixed routes, added resources :sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/api/v1/login", to: "api/v1/sessions#create"
-  post "/api/v1/signup", to: "api/v1/user#create"
-  post "/api/v1/newpost", to: "api/v1/posts#create"
-  get "/api/v1/profile", to: "api/v1/users#show"
-  get "/api/1/get_session", to: "api/v1/sessions#get_session"
-  delete "/api/v1/logout" to: "/api/v1/sessions#destroy"
-
+  
   namespace :api do 
     namespace :v1 do 
       resources :users
       resources :businesses
       resources :post
+      resources :sessions
+
+      post "/api/v1/signup", to: "/api/v1/users#create"
+      post "/api/v1/newpost", to: "/api/v1/posts#create"
+      get "/api/v1/profile", to: "/api/v1/users#show"
+      get "/api/1/get_session", to: "/api/v1/sessions#get_session"
+      post "/api/v1/login", to: "/api/v1/sessions#create"
+      delete "/api/v1/logout", to: "/api/v1/sessions#destroy"
+    
+    end
+
   end
-  
 end
